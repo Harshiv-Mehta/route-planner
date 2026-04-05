acb5e5dde19710796258a2f0314ff5c740b88f51
swararajendra750-bit <swararajendra750@gmail.com>
2026-04-05 15:58:49 +0530

    Bind Render server to 0.0.0.0

--- local-server.js
+++ local-server.js
@@ -397,6 +397,6 @@ const server = http.createServer(async (req, res) => {
   }
 });
 
-server.listen(PORT, "127.0.0.1", () => {
-  console.log(`MetroPath server running on http://127.0.0.1:${PORT}`);
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`MetroPath server running on http://0.0.0.0:${PORT}`);
 });
